9b54360401f046211e0955240dfde6f40fda59a6
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-02-26 17:19:16 -0300

    ✨  implement professional reducer

---
+++ src/redux/ducks/professional/index.ts
@@ -0,0 +1,25 @@
+import { Reducer } from 'redux';
+import { ProfessionalActions, ProfessionalData } from './types';
+
+const INITIAL_STATE: ProfessionalData = {
+  curriculum: '',
+  role: '',
+  description: '',
+};
+
+// Reducers
+
+export const professional: Reducer<ProfessionalData> = (
+  state = INITIAL_STATE,
+  action,
+) => {
+  switch (action.type) {
+    case ProfessionalActions.PROFESSIONAL_DATA:
+      return { ...state, ...action.payload };
+
+    default:
+      break;
+  }
+};
+
+export default professional;
