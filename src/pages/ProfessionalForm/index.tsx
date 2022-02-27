3c05bbe6445be0d25cff5dc5fe9d2ba98929b25a
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-02-27 15:23:25 -0300

    ✨ handle change input implementation

--- src/pages/ProfessionalForm/index.tsx
+++ src/pages/ProfessionalForm/index.tsx
@@ -3,16 +3,59 @@ import { Container } from './styles';
 import Header from '../../components/Header';
 import Input from '../../components/Input';
 import TextArea from '../../components/TextArea';
+import Button from '../../components/Button';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { ProfessionalData } from '../../redux/ducks/professional/types';
 
 const ProfessionalForm: React.FC = () => {
+  const INITIAL_STATE: ProfessionalData = {
+    curriculum: '',
+    description: '',
+    role: '',
+  };
+
+  const [professional, setProfessional] = useState(INITIAL_STATE);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setProfessional((prevState) => ({ ...prevState, [name]: value }));
+
+    console.log(name, value);
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
   return (
     <Container>
       <Header>Dados Profissionais</Header>
-      <fieldset>
-        <TextArea placeholder='13+ year exp in Typescript' />
-        <Input label='Role' placeholder='React Engineer' />
-        <Input label='Role Description' placeholder='Emotional Salary' />
-      </fieldset>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <TextArea
+            name='curriculum'
+            placeholder='13+ year exp in Typescript'
+            value={professional.curriculum}
+            onChange={handleChange}
+          />
+          <Input
+            label='Role'
+            name='role'
+            placeholder='React Engineer'
+            value={professional.role}
+            onChange={handleChange}
+          />
+          <Input
+            label='Role Description'
+            name='description'
+            placeholder='Emotional Salary'
+            value={professional.description}
+            onChange={handleChange}
+          />
+          <Button type='submit'>Enviar</Button>
+        </fieldset>
+      </form>
     </Container>
   );
 };
