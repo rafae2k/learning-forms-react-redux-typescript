3a5b4d95c7017d784914934775598d51238aecaf
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-02-27 15:19:52 -0300

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,21 @@ import React from 'react';
 import GlobalStyles from './Themes/GlobalStyles';
 
 import FormDataDisplay from './pages/FormDataDisplay';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PersonalForm from './pages/PersonalForm';
+import ProfessionalForm from './pages/ProfessionalForm';
 
 const App: React.FC = () => {
   return (
     <div className='App'>
-      <GlobalStyles />
-      <FormDataDisplay />
+      <BrowserRouter>
+        <GlobalStyles />
+        <Routes>
+          <Route path='/' element={<PersonalForm />} />
+          <Route path='/professionalform' element={<ProfessionalForm />} />
+          <Route path='/formdisplay' element={<FormDataDisplay />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
