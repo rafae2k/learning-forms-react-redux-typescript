79590cd27ac74640ef0bcbee9e98062f6165e518
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-02-27 15:22:30 -0300

    🎨  Style personal and professional form

--- src/pages/PersonalForm/styles.ts
+++ src/pages/PersonalForm/styles.ts
@@ -1,5 +1,18 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
 
+  justify-content: center;
+  align-items: center;
+
+  background-color: white;
+  padding: 2rem;
+
+  border-radius: 10px;
+
+  box-shadow: 0 20px 25px -5px rgb(0 0 0 / 0.1),
+    0 8px 10px -6px rgb(0 0 0 / 0.1);
 `;
