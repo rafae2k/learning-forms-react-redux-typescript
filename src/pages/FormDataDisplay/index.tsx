5aa05aac273c4a82f646d48e586d36736b4aa22f
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-02-27 15:59:10 -0300

    ✨  implementation formData

--- src/pages/FormDataDisplay/index.tsx
+++ src/pages/FormDataDisplay/index.tsx
@@ -1,34 +1,40 @@
-import { Container } from './styles';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
+// Components
 import Header from '../../components/Header';
 import Input from '../../components/Input';
 import TextArea from '../../components/TextArea';
+import { rootReducerState } from '../../redux/ducks';
+
+// Styles | styled-components
+import { Container } from './styles';
 
 const FormDataDisplay: React.FC = () => {
+  const [formState, setFormState] = useState({});
+
+  const { personal, profissional } = useSelector(
+    (state: rootReducerState) => state,
+  );
+
+  useEffect(() => {}, []);
+
   return (
     <Container>
       <Header>Dados Enviados</Header>
       <fieldset>
         <legend>Dados Pessoais</legend>
-        <Input
-          label='Nome'
-          placeholder='Pato da Silva'
-          value={''}
-          onChange={() => {}}
-        />
+        <Input label='Nome' placeholder='Pato da Silva' value={personal.name} />
         <Input
           label='Email'
           placeholder='pato@ducks.com'
-          value={''}
-          onChange={() => {}}
+          value={personal.email}
           type={'email'}
         />
         <Input
           label='CPF'
           placeholder='123.412.123-44'
-          value={''}
-          onChange={() => {}}
-          type={'text'}
+          value={personal.cpf}
           // pattern={
           //   '([0-9]{2}[.]?[0-9]{3}[.]?[0-9]{3}[/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[.]?[0-9]{3}[.]?[0-9]{3}[-]?[0-9]{2})'
           // }
@@ -36,19 +42,28 @@ const FormDataDisplay: React.FC = () => {
         <Input
           label='Endereço'
           placeholder='Rua dos quacks, 132'
-          value={''}
-          onChange={() => {}}
-          type={'text'}
+          value={personal.address}
         />
-        <Input label='Estado' placeholder='SP' />
-        <Input label='Cidade' placeholder='Patolândia' />
+        <Input label='Estado' placeholder='SP' value={personal.state} />
+        <Input label='Cidade' placeholder='Patolândia' value={personal.city} />
       </fieldset>
       <br />
       <fieldset>
         <legend>Dados Profissionais</legend>
-        <TextArea placeholder='13+ year exp in Typescript' />
-        <Input label='Role' placeholder='React Engineer' />
-        <Input label='Role Description' placeholder='Emotional Salary' />
+        <TextArea
+          placeholder='13+ year exp in Typescript'
+          value={profissional.curriculum}
+        />
+        <Input
+          label='Role'
+          placeholder='React Engineer'
+          value={profissional.role}
+        />
+        <Input
+          label='Role Description'
+          placeholder='Emotional Salary'
+          value={profissional.description}
+        />
       </fieldset>
     </Container>
   );
